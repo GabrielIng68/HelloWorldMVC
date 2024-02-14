be1260f0da1fe49ba42778ab453eef8c7b748741
Gabriel <gabriel.hauss.auditeur@lecnam.net>
2024-02-14 10:13:37 +0100

    Création du contenu main

--- model/src/main/java/helloworldmvc/model/Model.java
+++ model/src/main/java/helloworldmvc/model/Model.java
@@ -1,12 +1,23 @@
 package helloworldmvc.model;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 public class Model implements IModel {
     @Override
     public String getMessage() {
-        InputStream resourceAsStream = Model.class.getResourceAsStream("message.txt");
-        assert resourceAsStream != null;
-        return resourceAsStream.toString();
+        String s = "";
+        try {
+            InputStream inputStream= Model.class.getResourceAsStream("message.txt");
+            if( inputStream != null ) {
+                s = new BufferedReader(new InputStreamReader(inputStream))
+                        .lines().collect(Collectors.joining("\n"));
+            }
+        } catch (Exception e) {
+            System.out.println("\u001B[31m" + e + "\u001B[0m");
+        }
+        return s;
     }
 }
