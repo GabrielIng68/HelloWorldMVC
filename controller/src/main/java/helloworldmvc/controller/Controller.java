d8d3659019b9bd3d9f7832104bf2e6bd9895f3da
Gabriel <gabriel.hauss.auditeur@lecnam.net>
2024-02-14 10:03:04 +0100

    Création du contenu du module controleur

---
+++ controller/src/main/java/helloworldmvc/controller/Controller.java
@@ -0,0 +1,18 @@
+package helloworldmvc.controller;
+
+import helloworldmvc.model.IModel;
+import helloworldmvc.view.IView;
+
+public class Controller {
+    private IModel model;
+    private IView view;
+
+    public Controller(IModel model, IView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void run() {
+        view.display(model.getMessage());
+    }
+}
